d6e304d70e2f24230cfea08e640ad1befd25ac06
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-06-13 19:13:59 +0200

    NotFullDataInFactoryException

---
+++ 003-rabbitmq-start/app/src/Exception/NotFullDataInFactoryException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class NotFullDataInFactoryException
+ * @package App\Exception
+ */
+class NotFullDataInFactoryException extends RabbitMQTutorialException
+{
+}
