f3c70813cd9da0364139542f70013c599d55f4c3
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-06-14 16:41:12 +0200

    ComponentsEmptyException

---
+++ 003-rabbitmq-start/app/src/Exception/ComponentsEmptyException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class ComponentsEmptyException
+ * @package App\Exception
+ */
+class ComponentsEmptyException extends RabbitMQTutorialException
+{
+}
