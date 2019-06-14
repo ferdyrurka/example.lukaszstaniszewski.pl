14de38f8c2c5cdd1b90f6b9d4ea3c8abd4298f86
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-06-14 16:42:52 +0200

    CompositeStorageIsEmptyException

---
+++ 003-rabbitmq-start/app/src/Exception/CompositeStorageIsEmptyException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Exception;
+
+/**
+ * Class CompositeStorageIsEmptyException
+ * @package App\Exception
+ */
+class CompositeStorageIsEmptyException extends RabbitMQTutorialException
+{
+}
