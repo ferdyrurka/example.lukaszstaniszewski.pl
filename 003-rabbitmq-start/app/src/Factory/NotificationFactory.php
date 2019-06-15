12a538a27a7dcf8a9b57c7b1143cb3404d046cf9
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-06-15 10:53:41 +0200

    Added method chooseNotificationComponent

--- 003-rabbitmq-start/app/src/Factory/NotificationFactory.php
+++ 003-rabbitmq-start/app/src/Factory/NotificationFactory.php
@@ -3,7 +3,11 @@ declare(strict_types=1);
 
 namespace App\Factory;
 
+use App\Composite\RabbitMQ\Component\AlertNotificationComponent;
+use App\Composite\RabbitMQ\Component\NewMessageNotificationComponent;
+use App\Composite\RabbitMQ\RabbitMQComponentAbstract;
 use App\Entity\Notification;
+use App\Exception\InvalidArgsException;
 use App\Exception\NotFullDataInFactoryException;
 
 /**
@@ -26,5 +30,23 @@ class NotificationFactory
         $notification = new Notification();
         $notification->setMessage($arrayNotification['message']);
         $notification->setType($arrayNotification['type']);
+    }
+
+    /**
+     * @param string $type
+     * @param string $message
+     * @return RabbitMQComponentAbstract
+     * @throws InvalidArgsException
+     */
+    public function chooseNotificationComponent(string $type, string $message): RabbitMQComponentAbstract
+    {
+        switch ($type) {
+            case 'alert':
+                return new AlertNotificationComponent($message);
+            case 'new_message':
+                return new NewMessageNotificationComponent($message);
+            default:
+                throw new InvalidArgsException('Undefined type notification component');
+        }
     }
 }
