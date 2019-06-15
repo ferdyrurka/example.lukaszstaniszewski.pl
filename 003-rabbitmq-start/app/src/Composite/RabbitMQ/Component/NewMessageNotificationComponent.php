960e4b0511b8d85a412114f711db15a6aaf62dd2
Ferdyrurka <kontakt@lukaszstaniszewski.pl>
2019-06-15 09:35:28 +0200

    NewMessageNotificationComponent

---
+++ 003-rabbitmq-start/app/src/Composite/RabbitMQ/Component/NewMessageNotificationComponent.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Composite\RabbitMQ\Component;
+
+use App\Composite\RabbitMQ\RabbitMQComponentAbstract;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Message\AMQPMessage;
+
+/**
+ * Class NewMessageNotificationComponent
+ * @package App\Composite\RabbitMQ\Component
+ */
+class NewMessageNotificationComponent extends RabbitMQComponentAbstract
+{
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * NewMessageNotificationComponent constructor.
+     * @param string $message
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * @param AMQPChannel $AMQPChannel
+     */
+    public function run(AMQPChannel $AMQPChannel): void
+    {
+        $AMQPChannel->queue_declare('new_message_notification');
+
+        $message = new AMQPMessage(
+            \json_encode([
+                'message' => $this->message
+            ])
+        );
+
+        $AMQPChannel->basic_publish($message, '', 'new_message_notification');
+    }
+}
---
+++ 003-rabbitmq-start/app/tests/Composite/RabbitMQ/Component/NewMessageNotificationComponentTest.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Tests\Composite\RabbitMQ\Component;
+
+use App\Composite\RabbitMQ\Component\NewMessageNotificationComponent;
+use Mockery;
+use PhpAmqpLib\Channel\AMQPChannel;
+use PhpAmqpLib\Message\AMQPMessage;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class NewMessageNotificationComponentTest
+ * @package App\Tests\Composite\RabbitMQ\Component
+ */
+class NewMessageNotificationComponentTest extends TestCase
+{
+    use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
+
+    /**
+     * @test
+     * @runInSeparateProcess
+     */
+    public function runOk(): void
+    {
+        $amqpChannel = Mockery::mock(AMQPChannel::class);
+        $amqpChannel->shouldReceive('queue_declare')->withArgs(
+            [
+                'new_message_notification'
+            ]
+        )->once();
+        $amqpChannel->shouldReceive('basic_publish')->withArgs(
+            [
+                AMQPMessage::class,
+                '',
+                'new_message_notification'
+            ]
+        )
+            ->once()
+        ;
+
+        $amqpMessage = Mockery::mock('overload:' . AMQPMessage::class);
+        $amqpMessage->shouldReceive('__construct')->withArgs(
+            function (string $jsonData): bool {
+                $arrayData = \json_decode($jsonData, true);
+
+                return !(empty($arrayData) || !isset($arrayData['message']) ||
+                    $arrayData['message'] !== 'Hello World'
+                );
+            }
+        );
+
+        $newMessageNotificationComponent = new NewMessageNotificationComponent('Hello World');
+        $newMessageNotificationComponent->run($amqpChannel);
+    }
+}
